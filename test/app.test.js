85898eb50766d6340a0740a9a269edd75f6c2d84
Sam Jespersen <heyheysamj@gmail.com>
2019-12-02 11:44:51 -0800

    signin route

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/model/User');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -28,6 +29,18 @@ describe('app routes', () => {
           username: 'test'
         });
       });
+  });
+
+  it('can signin a user', async() => {
+    await User.create({ username: 'test', password: '1234' });
+    const res = await request(app)
+      .post('/api/v1/auth/signin')
+      .send({ username: 'test', password: '1234' });
+
+    expect(res.body).toEqual({
+      _id: expect.any(String),
+      username: 'test'
+    });
   });
 
 });
