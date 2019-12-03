fec3706ae17eea37b8cfb175c8329c025c759a67
Sam Jespersen <heyheysamj@gmail.com>
2019-12-03 15:37:11 -0800

    these tests dont work

--- test/url.test.js
+++ test/url.test.js
@@ -1,42 +1,56 @@
 require('dotenv').config();
-
 const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
-const User = require('../lib/model/User');
-
-describe('url route', () => {
+describe('url routes', () => {
   beforeAll(() => {
     connect();
   });
-
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
-
   afterAll(() => {
     return mongoose.connection.close();
   });
-
-  it('creates a url', async() => {
+  it('creates a custom url', async() => {
     const agent = request.agent(app);
-
     await agent
       .post('/api/v1/auth/signup')
       .send({ username: 'test', password: 'password' });
-
     return agent
-      .post('/api/v1/urls')
+      .post('/api/v1/url')
       .send({
-        customUrl: 'your-mom-42069',
-        longUrl: 'google.com'
+        customUrl: 'murmuring-chamber',
+        longUrl: 'https://www.google.com/'
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          customUrl: 'your-mom-42069',
-          longUrl: 'google.com',
+          customUrl: 'murmuring-chamber',
+          longUrl: 'https://www.google.com/',
+          shortUrl: expect.any(String),
+          hits: 0,
+          user: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+  it('creates a generated url', async() => {
+    const agent = request.agent(app);
+    await agent
+      .post('/api/v1/auth/signup')
+      .send({ username: 'test2', password: 'password' });
+    return agent
+      .post('/api/v1/url')
+      .send({
+        longUrl: 'https://www.google.com/'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          longUrl: 'https://www.google.com/',
+          shortUrl: expect.any(String),
           hits: 0,
           user: expect.any(String),
           __v: 0
